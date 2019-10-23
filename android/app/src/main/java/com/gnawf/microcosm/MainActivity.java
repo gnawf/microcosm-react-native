5e7d9da5ffd81cd41b9a373d558bea89f8d26eb1
Franklin Wang <fractalemagic@gmail.com>
2019-10-23 20:25:46 +1300

    Fix Android build

--- android/app/src/main/java/com/gnawf/microcosm/MainActivity.java
+++ android/app/src/main/java/com/gnawf/microcosm/MainActivity.java
@@ -1,23 +1,6 @@
 package com.gnawf.microcosm;
 
-import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import com.reactnativenavigation.NavigationActivity;
 
-public class MainActivity extends ReactActivity {
-  @Override
-  protected String getMainComponentName() {
-    return "Microcosm";
-  }
-
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-       return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
-    };
-  }
+public class MainActivity extends NavigationActivity {
 }
